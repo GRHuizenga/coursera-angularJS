3caae36151eb1f7a07d480046d67cd4718f753c1
GRHuizenga <huizengagr@gmail.com>
2017-11-19 15:04:35 +0100

    finished assignment

--- module2_solution/app.js
+++ module2_solution/app.js
@@ -3,16 +3,59 @@
 
 angular.module('ShoppingListCheckOff', [])
 	.controller('ToBuyController', ToBuyController)
-	.Controller('AlreadyBoughtController', AlreadyBoughtController);
+	.controller('AlreadyBoughtController', AlreadyBoughtController)
+	.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-ToBuyController.$inject = ['$scope'];
-function ToBuyController($scope) {
-	$scope.message = "Hello";
+ToBuyController.$inject = ['$scope', 'ShoppingListCheckOffService'];
+function ToBuyController($scope, ShoppingListCheckOffService) {
+	var toBuy = this;
+
+	ShoppingListCheckOffService.addItem('chocolate bars', 5);
+	ShoppingListCheckOffService.addItem('bread', 1);
+	ShoppingListCheckOffService.addItem('cookies', 10);
+	ShoppingListCheckOffService.addItem('cheese', 1);
+	ShoppingListCheckOffService.addItem('soda\'s', 2);
+
+	toBuy.items = ShoppingListCheckOffService.getItemsToBuy();
+
+	toBuy.buyItem = function(index) {
+		ShoppingListCheckOffService.buyItem(index);
+	}
 };
 
-AlreadyBoughtController.$inject = ['$scope'];
-function AlreadyBoughtController($scope) {
-	
+AlreadyBoughtController.$inject = ['$scope', 'ShoppingListCheckOffService'];
+function AlreadyBoughtController($scope, ShoppingListCheckOffService) {
+	var bought = this;
+
+	bought.items = ShoppingListCheckOffService.getItemsBought();
 };
+
+function ShoppingListCheckOffService() {
+	var service = this;
+
+	var itemsToBuy = [];
+	var itemsBought = [];
+
+	service.buyItem = function(index) {
+		let item = itemsToBuy[index];
+		itemsToBuy.splice(index, 1);
+		itemsBought.push(item);
+	}
+
+	service.addItem = function(itemName, quantity) {
+		itemsToBuy.push({
+			name: itemName,
+			quantity: quantity
+		});
+	}
+
+	service.getItemsToBuy = function() {
+		return itemsToBuy;
+	}
+
+	service.getItemsBought = function() {
+		return itemsBought;
+	}
+}
 
 })();
