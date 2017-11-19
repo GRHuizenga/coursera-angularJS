ffcf6c871f33ede558f64a393a77440b30b5e831
GRHuizenga <huizengagr@gmail.com>
2017-11-19 14:11:14 +0100

    angular includes

--- module2_solution/app.js
+++ module2_solution/app.js
@@ -1,6 +1,18 @@
 (function () {
 'use strict';
 
+angular.module('ShoppingListCheckOff', [])
+	.controller('ToBuyController', ToBuyController)
+	.Controller('AlreadyBoughtController', AlreadyBoughtController);
 
+ToBuyController.$inject = ['$scope'];
+function ToBuyController($scope) {
+	$scope.message = "Hello";
+};
+
+AlreadyBoughtController.$inject = ['$scope'];
+function AlreadyBoughtController($scope) {
+	
+};
 
 })();
